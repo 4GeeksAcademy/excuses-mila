6a76cb8dd951aae520e6debc956420eec38daec5
Milangela-wpz <milangela.1920@gmail.com>
2025-05-05 15:57:56 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ window.onload = function () {
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let when = ['before the class', 'when I was sleeping', 
+    'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  let random1 = who[excuse(who)];
-  let random2 = action[excuse(action)];
-  let random3 = what[excuse(what)];
-  let random4 = when[excuse(when)];
+  let quien = who[excuse(who)];
+  let accion = action[excuse(action)];
+  let que = what[excuse(what)];
+  let cuando = when[excuse(when)];
 
-  let excusa = random1 + " " + random2 + " " + random3 + " " + random4;
-
+  let excusa = `${quien} ${accion} $que} ${cuando}`
   document.getElementById("excuse").innerHTML = excusa;
 };
